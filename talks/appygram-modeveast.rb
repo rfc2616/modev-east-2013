787ed6c58742bf03c46f766c95e3a20f6884932e
Rob Heittman <rob.heittman@solertium.com>
2013-12-13 00:44:42 -0500

    WIP MoDevEast talk

---
+++ talks/appygram-modeveast.rb
@@ -0,0 +1,155 @@
+# encoding: utf-8
+
+center <<-EOS
+  \e[1mOpening next-level dialogue with native app users via Appygram\e[0m
+
+
+  Rob Heittman
+  @rfc2616
+
+  MoDevEast 2013
+EOS
+
+block <<-EOS
+  Your native app user experienced a problem.
+
+  What should they do?
+
+  A) Suffer
+  B) Post a nasty review on the App Store
+  C) Engage with someone who can help them
+EOS
+
+block <<-EOS
+  What kind of response does your user want?
+
+  A) An instant, polite, useless autoresponder
+  B) A detailed inquisition about their experience
+  C) Actual help from someone who understands
+EOS
+
+center <<-EOS
+  \e[1mThis shouldn't be hard\e[0m
+EOS
+
+center <<-EOS
+  \e[1mWhy is this so hard?\e[0m
+EOS
+
+block <<-EOS
+     many possible responders
+
+  Inhouse            Outsourced
+  -------            ----------
+
+  - Helpdesk         - Developers
+  - Operations       - Cloud/MBAAS
+  - IT               - API Providers
+  - Marketing        - Consultants
+EOS
+
+block <<-EOS
+         using many systems
+
+  - GitHub Issues         - ZenDesk
+  - Desk.com              - Assembla
+  - Email                 - Basecamp
+EOS
+
+center <<-EOS
+  \e[1mAnd the rules keep changing...\e[0m
+
+
+EOS
+
+center <<-EOS
+  And the rules keep changing...
+
+  \e[1mand getting more complex...\e[0m
+EOS
+
+block <<-EOS
+  We got tired of making ad-hoc solutions
+  for this problem over and over again.
+EOS
+
+block <<-EOS
+  So we made this.
+EOS
+
+image 'appy-screen1.png'
+
+center <<-EOS
+  Appygram provides a smart back end for
+  feedback features you build into your
+  native mobile app.
+
+  It stores a rolling buffer of recent
+  messages, and forwards messages on to
+  the services you specify.
+EOS
+
+center <<-EOS
+  ** ADVANCED **
+
+  Appygram also collects and forwards
+  App Store and Google Play reviews
+EOS
+
+center <<-EOS
+  *** SUPER ADVANCED ***
+
+  Appygram can create and route alerts
+  about unusual or exceptional conditions
+  detected by automated systems.
+EOS
+
+block <<-EOS
+  Like ...
+
+  - huh, we're getting a lot of exceptions
+  - something expected stopped happening
+EOS
+
+block <<-EOS
+  SPOILERS!
+
+  Where it really gets interesting is
+  when we can correlate the two. Users are
+  writing in with problems AND some
+  monitored conditions are outside of
+  normal parameters.
+EOS
+
+center <<-EOS
+  Appygram is free for personal accounts.
+
+  Just go to \e[1mwww.appygram.com\e[0m to sign up.
+
+  The "freegan" plan will get you going.
+EOS
+
+block <<-EOS
+  What you don't get as a freegan:
+
+  - Lots and lots of distinct apps
+  - Ability to email third parties
+  - Ability to delegate access to your apps
+    to "watchers"
+EOS
+
+block <<-EOS
+  What everybody gets on any plan:
+
+  - High availability SSL backend
+    compatible with all major mobile devices
+  - Fully configurable webhook and email
+    forwarders
+EOS
+
+block <<-EOS
+  Coming down the road
+
+  - Support for web-based feedback forms
+EOS
+
